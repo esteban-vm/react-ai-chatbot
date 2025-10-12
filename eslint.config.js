e521a2a0f78fb3efd565b491dd19f99e8b43bd30
Esteban V.M. <estebanvm90@outlook.com>
2025-10-12 09:31:06 -0300

    update eslint config
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,4 @@
 import js from '@eslint/js'
-import prettier from 'eslint-plugin-prettier/recommended'
 import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
@@ -97,12 +96,6 @@ export default defineConfig([
           sortExportKindFirst: 'type',
         },
       ],
-    },
-  },
-  prettier,
-  {
-    rules: {
-      'prettier/prettier': ['warn'],
     },
   },
 ])
