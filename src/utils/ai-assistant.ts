258173329391252ae99d0b023d6ccf7c65e363f1
Esteban V.M. <estebanvm90@outlook.com>
2025-10-13 22:19:35 -0300

    create ai assistant
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

---
+++ src/utils/ai-assistant.ts
@@ -0,0 +1,16 @@
+import type { Chat } from '@google/genai'
+import { GoogleGenAI } from '@google/genai'
+
+export const AIAssistant = class {
+  #chat: Chat
+
+  constructor(model: string) {
+    const ai = new GoogleGenAI({ apiKey: import.meta.env.VITE_GEMINI_API_KEY })
+    this.#chat = ai.chats.create({ model })
+  }
+
+  async chat(content: string) {
+    const response = await this.#chat.sendMessage({ message: content })
+    return response.text
+  }
+}
