c9ebdddb85fb21a772b1da13cb53cb8e793dc893
Esteban V.M. <estebanvm90@outlook.com>
2025-10-14 21:54:41 -0300

    fix message item style
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/components/message-item/message-item.styled.ts
+++ src/components/message-item/message-item.styled.ts
@@ -3,7 +3,7 @@ import tw from 'tailwind-styled-components'
 export const Container = tw.div`
   prose
   w-[90%]
-  rounded-l-lg
   px-3
   py-1.5
+  leading-5
 `
--- src/components/message-item/message-item.tsx
+++ src/components/message-item/message-item.tsx
@@ -4,7 +4,7 @@ import * as $ from './message-item.styled'
 
 export function MessageItem({ role, content }: Message) {
   return (
-    <$.Container className={role === 'user' ? 'self-end bg-neutral-100' : 'self-start'}>
+    <$.Container className={role === 'user' ? 'self-end rounded-l-lg bg-neutral-100' : 'self-start'}>
       <Markdown remarkPlugins={[remarkGfm]}>{content}</Markdown>
     </$.Container>
   )
