8fe77e7b9a6ad851645bd9da66e6f3c7b3a7dc55
Esteban V.M. <estebanvm90@outlook.com>
2025-10-20 09:35:16 -0300

    update styles for code highlighter
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/components/atoms/code-highlighter.tsx
+++ src/components/atoms/code-highlighter.tsx
@@ -1,13 +1,15 @@
 import type { SyntaxHighlighterProps } from 'react-syntax-highlighter'
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
-import { oneDark } from 'react-syntax-highlighter/dist/esm/styles/prism'
+import { oneDark, oneLight } from 'react-syntax-highlighter/dist/esm/styles/prism'
+import { useThemeStore } from '@/hooks'
 
 export function CodeHighlighter(props: SyntaxHighlighterProps) {
+  const { theme } = useThemeStore()
+
   return (
     <SyntaxHighlighter
-      PreTag='div'
       codeTagProps={{ style: { whiteSpace: 'pre-wrap', fontSize: '0.75rem' } }}
-      style={oneDark}
+      style={theme === 'dark' ? oneDark : oneLight}
       showLineNumbers
       {...props}
     />
