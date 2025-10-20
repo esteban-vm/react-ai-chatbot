69f2ae61452be231f6251b00bc443f68746153b9
Esteban V.M. <estebanvm90@outlook.com>
2025-10-20 09:29:12 -0300

    update styles for loading spinner
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/components/molecules/loading-spinner/loading-spinner.tsx
+++ src/components/molecules/loading-spinner/loading-spinner.tsx
@@ -1,10 +1,13 @@
 import { BeatLoader } from 'react-spinners'
+import { useThemeStore } from '@/hooks'
 import * as $ from './loading-spinner.styled'
 
 export function LoadingSpinner() {
+  const { theme } = useThemeStore()
+
   return (
     <$.Container>
-      <BeatLoader aria-label='Loading Spinner' />
+      <BeatLoader aria-label='Loading Spinner' color={theme === 'dark' ? '#e5e5e5' : '#262626'} />
     </$.Container>
   )
 }
