5cde721d8062127857776805bf192d3a9cb3b8c8
Esteban V.M. <estebanvm90@outlook.com>
2025-10-21 15:22:04 -0300

    update message list
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/components/organisms/message-list/message-list.tsx
+++ src/components/organisms/message-list/message-list.tsx
@@ -1,15 +1,23 @@
+import { useTranslation } from 'react-i18next'
 import { Molecules } from '@/components'
 import { useMessageList, useMessageStore } from '@/hooks'
-import { MOCK_MESSAGE_GROUPS, WELCOME_MESSAGE_GROUP } from '@/utils'
 import * as $ from './message-list.styled'
 
 export function MessageList() {
   const { isLoading, messages } = useMessageStore()
+  const { t } = useTranslation('translation', { keyPrefix: 'message_list' })
 
   const { ref, groups } = useMessageList({
     shouldScroll: !isLoading,
     messageList: messages,
   })
+
+  const WELCOME_MESSAGE_GROUP: Message[] = [
+    {
+      role: 'system',
+      content: `**${t('welcome_message')}**`,
+    },
+  ]
 
   return (
     <$.Container>
@@ -17,7 +25,7 @@ export function MessageList() {
         <Molecules.LoadingSpinner />
       ) : (
         <>
-          {[WELCOME_MESSAGE_GROUP, ...MOCK_MESSAGE_GROUPS, ...groups].map((messages) => {
+          {[WELCOME_MESSAGE_GROUP, ...groups].map((messages) => {
             return (
               <$.MessageGroup key={crypto.randomUUID()}>
                 {messages.map((message) => (
