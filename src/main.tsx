a3cb7001fe6f8a1e305008185dd008c026a1d202
Esteban V.M. <estebanvm90@outlook.com>
2025-10-23 09:13:42 -0300

    update main.tsx
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from '@/App'
-import '@/index.css'
 import '@/i18n/config'
+import '@/styles/globals.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
