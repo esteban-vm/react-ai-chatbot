8107a174e7e5641d635a9166c6c85a486554892e
Esteban V.M. <estebanvm90@outlook.com>
2025-10-28 12:06:21 -0300

    update message list
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/components/organisms/message-list/message-list.tsx
+++ src/components/organisms/message-list/message-list.tsx
@@ -1,41 +1,55 @@
+import { useEffect, useMemo } from 'react'
 import { useTranslation } from 'react-i18next'
 import { Molecules } from '@/components'
 import { mockGroups } from '@/constants'
-import { useMessageList, useMessageStore } from '@/hooks'
+import { useMessageStore } from '@/hooks'
 import { ChatMessage } from '@/utils'
 import * as $ from './message-list.styled'
 
 export function MessageList() {
   const { isLoading, isError, messages } = useMessageStore()
   const { t } = useTranslation('translation', { keyPrefix: 'message_list' })
-  const { ref, groups } = useMessageList({ messages, shouldScroll: !isLoading })
+
+  const realGroups: ChatMessage[][] = useMemo(() => {
+    return messages.reduce<ChatMessage[][]>((groups, message) => {
+      if (message.role === 'user') groups.push([])
+      groups.at(-1)?.push(message)
+      return groups
+    }, [])
+  }, [messages])
 
   const welcomeMessage = ChatMessage.create('system', `**${t('welcome_message')}**`)
-  const messageGroups: ChatMessage[][] = [[welcomeMessage], ...mockGroups, ...groups]
+  const messageGroups: ChatMessage[][] = [[welcomeMessage], ...mockGroups, ...realGroups]
 
   if (isError) {
     const errorMessage = ChatMessage.create('system', `**${t('error_message')}**`)
     messageGroups.push([errorMessage])
   }
 
+  useEffect(() => {
+    if (!isLoading) {
+      const lastMessageGroup = document.querySelector<HTMLElement>('article[data-message]:last-of-type')
+
+      if (lastMessageGroup?.dataset.message) {
+        lastMessageGroup.scrollIntoView({ behavior: 'smooth' })
+      }
+    }
+  }, [isLoading])
+
   return (
     <$.Container>
       {isLoading ? (
         <Molecules.LoadingSpinner />
       ) : (
-        <>
-          {messageGroups.map((messages, index) => {
-            return (
-              // eslint-disable-next-line react/no-array-index-key
-              <$.MessageGroup key={index}>
-                {messages.map((message) => (
-                  <Molecules.MessageItem key={message.id} message={message} />
-                ))}
-              </$.MessageGroup>
-            )
-          })}
-          <$.ListEnding ref={ref} />
-        </>
+        messageGroups.map((messages) => {
+          return (
+            <$.MessageGroup key={crypto.randomUUID()} data-message>
+              {messages.map((message) => (
+                <Molecules.MessageItem key={message.id} message={message} />
+              ))}
+            </$.MessageGroup>
+          )
+        })
       )}
     </$.Container>
   )
