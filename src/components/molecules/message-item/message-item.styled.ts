87b185de5a509c752fc611c2e422e194ab756128
Esteban V.M. <estebanvm90@outlook.com>
2025-10-30 23:10:40 -0300

    style improvements
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/components/molecules/message-item/message-item.styled.ts
+++ src/components/molecules/message-item/message-item.styled.ts
@@ -2,12 +2,17 @@ import tw from 'tailwind-styled-components'
 
 export const Container = tw.div`
   prose
+  prose-sm
   w-[90%]
   rounded-xl
   bg-neutral-200
-  p-3
-  text-[90%]
+  px-3
+  py-1.5
+  text-justify
   leading-5
   dark:bg-neutral-800
   dark:prose-invert
+  prose-hr:my-2.5
+  prose-hr:border-neutral-100
+  dark:prose-hr:border-neutral-900
 `
--- src/components/organisms/message-list/message-list.styled.ts
+++ src/components/organisms/message-list/message-list.styled.ts
@@ -7,8 +7,11 @@ export const Container = tw.div`
   flex-col
   overflow-y-auto
   rounded-xl
+  border
+  border-neutral-300
   bg-neutral-100
   p-0.5
+  dark:border-neutral-700
   dark:bg-neutral-900
 `
 
