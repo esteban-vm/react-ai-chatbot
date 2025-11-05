4bc8526a638dfebd2ef0ca70df2e838b72655c31
Esteban V.M. <estebanvm90@outlook.com>
2025-11-04 22:16:22 -0300

    update eslint rules
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- eslint.config.js
+++ eslint.config.js
@@ -35,6 +35,7 @@ export default defineConfig([
       },
     },
     rules: {
+      'no-unused-vars': 'off',
       'react/prop-types': 'off',
       'react/react-in-jsx-scope': 'off',
       'react/no-array-index-key': 'warn',
